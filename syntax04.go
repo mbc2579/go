0cefa149117f77713c2ebf3dde2867c3845c0235
mbc2579 <mbc2579@naver.com>
2021-09-19 18:53:19 +0900

    난수 추출된 수의 소수 판정 프로그램 v0.1

--- syntax04.go
+++ syntax04.go
@@ -7,29 +7,29 @@ import (
 )
 
 // 난수 추출된 수의 소수 판정 프로그램 v0.6
-func main() {
-	// seed 설정
-	seed := time.Now().Unix()
-	rand.Seed(seed)
+// func main() {
+// 	// seed 설정
+// 	seed := time.Now().Unix()
+// 	rand.Seed(seed)
 
-	isPrime := true
-	number := rand.Intn(150) + 2
-	//number = 21
-	fmt.Println("임의로 추출된 수 : ", number)
+// 	isPrime := true
+// 	number := rand.Intn(150) + 2
+// 	//number = 21
+// 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 2; i < number; i++ {
-		if number%i == 0 {
-			isPrime = false
-			break // 첫 번째 약수가 발견되면 반복문 즉시 종료
-		}
-		//fmt.Print(i, " ")
-	}
-	if isPrime { // 비교 연산자 제거
-		fmt.Println(number, "는 소수입니다!")
-	} else {
-		fmt.Println(number, "는 소수가 아닙니다!")
-	}
-}
+// 	for i := 2; i < number; i++ {
+// 		if number%i == 0 {
+// 			isPrime = false
+// 			break // 첫 번째 약수가 발견되면 반복문 즉시 종료
+// 		}
+// 		//fmt.Print(i, " ")
+// 	}
+// 	if isPrime { // 비교 연산자 제거
+// 		fmt.Println(number, "는 소수입니다!")
+// 	} else {
+// 		fmt.Println(number, "는 소수가 아닙니다!")
+// 	}
+// }
 
 // package main
 
@@ -132,29 +132,29 @@ func main() {
 // 	"time" // seed 생성용 패키지
 // )
 
-// // 난수 추출된 수의 소수 판정 프로그램 v0.1
-// // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
-// func main() {
-// 	// seed 설정
-// 	seed := time.Now().Unix()
-// 	rand.Seed(seed)
+// 난수 추출된 수의 소수 판정 프로그램 v0.1
+// 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
+func main() {
+	// seed 설정
+	seed := time.Now().Unix()
+	rand.Seed(seed)
 
-// 	count := 0
-// 	number := rand.Intn(150) + 2 // 0과 1제외, 2 ~ 151 사이의 수
-// 	fmt.Println("임의로 추출된 수 : ", number)
+	count := 0
+	number := rand.Intn(150) + 2 // 0과 1제외, 2 ~ 151 사이의 수
+	fmt.Println("임의로 추출된 수 : ", number)
 
-// 	for i := 1; i < number; i++ {
-// 		if number%i == 0 {
-// 			count++
-// 		}
-// 	}
+	for i := 1; i < number; i++ {
+		if number%i == 0 {
+			count++
+		}
+	}
 
-// 	if count == 2 {
-// 		fmt.Println(number, "는 소수입니다!")
-// 	} else {
-// 		fmt.Println(number, "는 소수가 아닙니다!")
-// 	}
-// }
+	if count == 2 {
+		fmt.Println(number, "는 소수입니다!")
+	} else {
+		fmt.Println(number, "는 소수가 아닙니다!")
+	}
+}
 
 // package main
 
